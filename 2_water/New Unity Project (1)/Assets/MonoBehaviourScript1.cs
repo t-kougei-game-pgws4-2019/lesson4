1dfcb358979ec2bdf51bb338e43526bb045f42e6
p156 <yuuichi714@yahoo.co.jp>
2019-10-28 07:27:36 +0900

    二つ目　多分途中？

---
+++ 2_water/New Unity Project (1)/Assets/MonoBehaviourScript1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoBehaviourScript1 : MonoBehaviour
+{
+    [SerializeField]
+    CustomRenderTexture _texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Material m = _texture.material;
+        m.SetFloat("_BulletX", -1.0f);
+        m.SetFloat("_BulletZ", -1.0f);
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+
+                Vector3 p = new Vector3(hit.point.x * 0.2f, 0f, hit.point.z * 0.2f);
+                float u = p.x * 0.5f + 0.5f;
+                float v = p.z * 0.5f + 0.5f;
+
+                u -= 0.5f;
+                v -= 0.5f;
+                m.SetFloat("_BulletX", u);
+                m.SetFloat("_BulletZ", v);
+
+                Debug.Log(u);
+                Debug.Log(v);
+
+            }
+        }
+    }
+}
---
+++ 2_water/New Unity Project (1)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshFilter>().mesh = generateFineMesh(100, 100, 10.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Mesh generateFineMesh(int nX, int nZ, float sizX, float sizZ)
+    {
+        int numVerticies = (nX + 1) * (nZ + 1);
+        Vector3[] aVertex = new Vector3[numVerticies];
+        Vector3[] aNormal = new Vector3[numVerticies];
+        Vector2[] aUv = new Vector2[numVerticies];
+
+        float dX = sizX / (float)nX;
+        float dZ = sizX / (float)nZ;
+        int idx = 0;
+        float fz = -0.05f * sizZ;
+
+        for (int z = 0; z <= nZ; z++)
+        {
+            float v = (float)z / (float)nX;
+            float fx = -0.05f * sizX;
+
+            for (int x = 0; x <= nX; x++)
+            {
+                float u = (float)x / (float)nX;
+                aVertex[idx] = new Vector3(fx, 0f, fz);
+                aNormal[idx] = new Vector3(0f, 1f, 0f);
+                aUv[idx] = new Vector2(u, v);
+                idx++;
+                fx += dX;
+            }
+            fz += dZ;
+        }
+
+        idx = 0;
+
+        int[] indecies = new int[nX * nZ * 6];
+
+        for (int z = 0; z < nZ; z++)
+        {
+            int index = z * (nX + 1);
+
+            for (int x = 0; x < nX; x++)
+            {
+                indecies[idx + 0] = index;
+                indecies[idx + 1] = index + nX + 1;
+                indecies[idx + 2] = index + 1;
+                indecies[idx + 3] = index + 1;
+                indecies[idx + 4] = index + nX + 1;
+                indecies[idx + 5] = index + nX + 2;
+                index++;
+                idx += 6;
+            }
+        }
+
+        Mesh mesh = new Mesh
+        {
+            vertices = aVertex,
+            normals = aNormal,
+            uv = aUv
+        };
+
+        mesh.SetIndices(indecies, MeshTopology.Triangles, 0);
+
+        return mesh;
+    }
+}
